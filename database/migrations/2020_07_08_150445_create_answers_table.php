ae6642f0f2d2bf0bc240903e22e1967288ffa2b8
Fadil S Hardy <xaber255@gmail.com>
2020-07-08 22:24:15 +0700

    add questions & answers migration

---
+++ database/migrations/2020_07_08_150445_create_answers_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('content');
+            $table->boolean('is_right_answer');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('question_id');
+
+            $table->foreign('question_id')
+                ->references('id')
+                ->on('pertanyaans')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
