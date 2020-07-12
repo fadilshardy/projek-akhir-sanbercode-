7242f83f5313d77fe9f131c78a066e0cec1fe7a5
fearhunt <firhanmahdi@gmail.com>
2020-07-12 11:18:29 +0700

    vote and delete

--- resources/views/answers/partials/_vote_form.blade.php
+++ resources/views/answers/partials/_vote_form.blade.php
@@ -1,23 +1,31 @@
 <li>
     <form action="/jawaban/{{$jawaban->id}}/{{$jawaban->vote_status() ? 'unvote/upvote' : 'upvote'}}" method="POST">
         @csrf
-        <i class="fa fa-caret-up mr-1 question-title" aria-hidden="true"></i>
-        <button class="question-title border-0 {{$jawaban->vote_status() ? 'bg-success' : ''}}">
+        <div class="upvote {{$jawaban->vote_status() ? 'voted' : ''}}">
+            <div>
+                <i class="fa fa-caret-up ml-1 mr-1 question-title" aria-hidden="true"></i>
+        <button class="question-title border-0 {{$jawaban->vote_status() ? '' : ''}}">
             
             Upvote
             ({{$jawaban->upvote_count()}})
         </button>
+            </div>
+        </div>
     </form>
 </li>
 <li>
     <form action="/jawaban/{{$jawaban->id}}/{{$jawaban->vote_status() === 0 ? 'unvote/downvote ' : 'downvote'}}"
         method="POST">
         @csrf
-        <i class="fa fa-caret-down red-gradient" aria-hidden="true"></i>
-        <button class="red-gradient border-0 {{$jawaban->vote_status() === 0 ? 'bg-danger' : ''}}">
-            Downvote
-            {{$jawaban->downvote_count()}}
-            
-        </button>
+        <div class="downvote {{$jawaban->vote_status() === 0 ? 'voted' : ''}}" >
+            <div>
+                <i class="fa fa-caret-down ml-1 red-gradient" aria-hidden="true"></i>
+                <button class="red-gradient border-0 {{$jawaban->vote_status() === 0 ? '' : ''}}">
+                    Downvote
+                    {{$jawaban->downvote_count()}}
+                    
+                </button>
+            </div>
+        </div>
     </form>
 </li>
--- resources/views/questions/comment/show.blade.php
+++ resources/views/questions/comment/show.blade.php
@@ -1,8 +1,9 @@
 <button type="button" class="btn btn-sm btn-light shadow-sm mb-2 ">
     <a href="/profile/{{$comment->user->id}}"><span class="badge badge-dark">{{$comment->user->name}}</span></a> :
     {{$comment->content}}
-</button>&nbsp;<small class="font-italic text-muted">{{$comment->updated_at->diffForHumans()}}</small>@if ($comment->user_id==Auth::user()->id)
-<form action="/pertanyaan/{{$comment->id}}/delete_comment/" method="POST">
+</button>&nbsp;
+<small class="font-italic d-inline-block text-muted">{{$comment->updated_at->diffForHumans()}}</small>@if ($comment->user_id==Auth::user()->id)&nbsp;
+<form class="d-inline-block" action="/pertanyaan/{{$comment->id}}/delete_comment/" method="POST">
     @csrf
     @method('DELETE')
     <input type="hidden" name="question_id" value="{{$question->id}}">
