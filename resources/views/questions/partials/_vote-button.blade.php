0096f074613298545b6a234297720c93d4e9ed74
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-07-12 15:09:43 +0700

    perbaiki vote pertanyaan -fitra

--- resources/views/questions/partials/_vote-button.blade.php
+++ resources/views/questions/partials/_vote-button.blade.php
@@ -27,18 +27,18 @@
 
         <div class="col-sm-6 text-lg-left my-1">
             @if($item->is_author() || Auth::guest())
-            <button class="btn btn-md btn-vote {{$item->vote_status() === 0 ? 'bg-danger' : ''}}" @guest
+            <button class="btn btn-md btn-vote {{!is_null($item->vote_status()) === 0 ? 'bg-danger' : ''}}" @guest
                 onclick="alert('Login terlebih dahulu!')" @endguest>
                 <h5>{{$item->downvote_count()}}</h5>
                 <i class="fa fa-caret-down" aria-hidden="true"></i>
             </button>
             @else
             @auth
-            <form action="/pertanyaan/{{$item->id}}/{{$item->vote_status() === 0 ? 'unvote/downvote ' : 'downvote'}}"
+            <form action="/pertanyaan/{{$item->id}}/{{$item->vote_status() ? 'unvote/downvote ' : 'downvote'}}"
                 method="POST">
                 @csrf
                 @endauth
-                <button class="btn btn-md btn-vote {{$item->vote_status() === 0 ? 'bg-danger' : ''}}">
+                <button class="btn btn-md btn-vote {{!is_null($item->vote_status()) ? 'bg-danger' : ''}}">
                     <h5>{{$item->downvote_count()}}</h5>
                     <i class="fa fa-caret-down" aria-hidden="true"></i>
                 </button>
